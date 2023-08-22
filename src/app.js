2c8832b9fda427397beeb3ede5dafd962be6a27b
Ayushman Jha <ayushpandit8188@gmail.com>
2023-08-23 01:08:16 +0530

    chunking of the <About /> component

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Components/header";
 import Body from "./Components/Body";
 import Footer from "./Components/Footer";
 
-import About from "./Components/About";
+//lazy loaded the about component
+const About = lazy(() => import("./Components/About"));
+//import About from "./Components/About";
 import Contact from "./Components/Contact";
 import RestrauntMenu from "./Components/RestrauntMenu";
 import Error from "./Components/error";
@@ -14,6 +16,8 @@ import ProfileClass from "./Components/ProfileClass";
 
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import ProfileFunction from "./Components/ProfileFunctional";
+import { lazy } from "react";
+import Shimmer from "./Components/ShimmerUI";
 
 const heading = <h1 id="title">Namaste 🙏 React</h1>;
 
@@ -40,7 +44,11 @@ const Applayout = createBrowserRouter([
       },
       {
         path: "/about",
-        element: <About />,
+        element: (
+          <Suspense fallback={<Shimmer />}>
+            <About />
+          </Suspense>
+        ),
         children: [
           {
             path: "profile",
